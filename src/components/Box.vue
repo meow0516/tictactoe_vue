bf86dda117a3716b5f30269a13a52ef6a5e22ecd
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-27 17:56:25 +0800

    Box.vue: update box template, css of hover

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,9 +1,18 @@
+<template>
+  <div class="ox_box col-4 border-1" @click="markBox">
+    <div class="inner_ox_box flex justify-content-center align-items-center h-6rem text-4xl"></div>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { ref } from "vue";
 
 defineProps<{ msg: string }>();
 
 const count = ref(0);
+function markBox() {
+  console.log('mark');
+}
 </script>
 
 <template>
@@ -22,3 +31,10 @@ const count = ref(0);
 </template>
 
 <style scoped></style>
+<style scoped>
+.ox_box:hover {
+  background-color: rgb(130, 230, 230);
+  cursor: pointer;
+}
+</style>
+
