6316ef2b2d6b88f6327c349af76da9b6bcb8df08
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-27 17:41:09 +0800

    app.vue: import TicTacToe.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import TicTacToe from './components/TicTacToe.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <TicTacToe />
 </template>
 
 <style>
@@ -17,5 +14,8 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+  box-sizing: border-box;
 }
 </style>
